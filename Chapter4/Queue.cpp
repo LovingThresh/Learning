b630f30dcc02928c74b7de3d557ef94ac84b721f
liuye <csu1704liuye@163.com>
2023-12-01 09:46:12 +0800

    Update

--- Chapter4/Queue.cpp
+++ Chapter4/Queue.cpp
@@ -1,5 +1,6 @@
 #include "Queue.h"
-
+#include <cstdlib>
+#include <ctime>
 void Customer::set(const long when) {
     arrive = when;
     processtime = std::rand() % 3 + 1;
--- Chapter4/Queue.h
+++ Chapter4/Queue.h
@@ -1,8 +1,7 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 // #include <random>
-#include <cstdlib>
-#include <ctime>
+
 class Customer
 {
 private:
